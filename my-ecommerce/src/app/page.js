7225cdc09f042f7c434065d253f0b066e979e198
OscarDME <oscarmadriz25@gmail.com>
2024-07-12 20:11:57 -0600

    Project

---
+++ my-ecommerce/src/app/page.js
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <div className = "p-5">
+      <div>
+        <h2 className = "text-2xl">Mobiles</h2>
+        <div className="py-4">
+          <div className="w-64">
+            <div className = "bg-blue-100 p-5 runded-xl">
+              <img src="/products/iphone.png" alt="phone" />
+            </div>
+            <div className = "mt-2">
+              <h3 className = "font-bold text-lg">Iphone 14 Pro</h3>
+            </div>
+            <p className = "text-sm mt-1 leading-4">lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quod.</p>
+            <div className = "flex mt-1">
+              <div className = "text-2xl font-bold grow">
+                $899
+              </div>
+              <button className = "bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
